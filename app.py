8567d82e51bd0c9180d572bfe7de5f2439114f14
penguin-penpen <penguingohack@outlook.com>
2016-05-03 23:06:52 +0800

    post, get test

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from app import app, api
-from app.api import HelloWorld
+from app.api import HelloWorld, TodoSimple
 
 api.add_resource(HelloWorld, '/')
-
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/api.py
+++ app/api.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+
+from flask import request
 from app import app, api
 from flask_restful import Resource, Api
 
@@ -6,5 +8,13 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+todos = {}
 
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
 
